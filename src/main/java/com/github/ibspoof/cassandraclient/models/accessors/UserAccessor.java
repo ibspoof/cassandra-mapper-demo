5ce8729f1405f49729513de211dadf078a5717eb
Brad Vernon <brad.vernon@datastax.com>
2017-06-11 16:44:12 -0700

    Updated Accessor

--- src/main/java/com/github/ibspoof/cassandraclient/models/accessors/UserAccessor.java
+++ src/main/java/com/github/ibspoof/cassandraclient/models/accessors/UserAccessor.java
@@ -4,11 +4,13 @@ package com.github.ibspoof.cassandraclient.models.accessors;
 import com.datastax.driver.mapping.Result;
 import com.datastax.driver.mapping.annotations.Accessor;
 import com.datastax.driver.mapping.annotations.Query;
+import com.datastax.driver.mapping.annotations.QueryParameters;
 import com.github.ibspoof.cassandraclient.models.User;
 
 @Accessor
 public interface UserAccessor {
 
     @Query("SELECT * FROM users")
+    @QueryParameters(consistency="LOCAL_ONE")
     Result<User> getAll();
 }
